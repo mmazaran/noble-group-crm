28f6073fb935d48871ec02f948d946b2bf31674d
Matt Mazaran <mattmazaran@mac.mynetworksettings.com>
2026-07-05 19:57:00 -0400

    Provide helpful error messages for missing env variables in server and browser clients

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,10 +3,16 @@ import { cookies } from "next/headers";
 
 export function createClient() {
   const cookieStore = cookies();
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Database Client Setup Failed: NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables are missing.");
+  }
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseAnonKey,
     {
       cookies: {
         get(name: string) {
